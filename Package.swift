6e39a725541dcaec174410bd2a72c18379ff8a4b
Sereivoan Yong <sereivoanyong@gmail.com>
2024-10-09 13:06:04 +0700

    Bump swift-tools-version to 5.10

--- Package.swift
+++ Package.swift
@@ -1,26 +1,16 @@
-// swift-tools-version:5.3
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version:5.10
+
 import PackageDescription
 
 let package = Package(
     name: "YPImagePicker",
     defaultLocalization: "en",
-    platforms: [
-        .iOS(.v12)
-    ],
     products: [
         .library(name: "YPImagePicker", targets: ["YPImagePicker"])
     ],
     dependencies: [
-        .package(
-            url: "https://github.com/freshOS/Stevia",
-            .exact("5.1.2")
-        ),
-        .package(
-            url: "https://github.com/HHK1/PryntTrimmerView",
-            .exact("4.0.2")
-        )
-
+        .package(url: "https://github.com/freshOS/Stevia", exact: "5.1.2"),
+        .package(url: "https://github.com/HHK1/PryntTrimmerView", exact: "4.0.2")
     ],
     targets: [
         .target(
